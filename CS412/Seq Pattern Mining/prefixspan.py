7d09d3aa5287abc6efd8aa32839e8afaa316d5fc
Pradyumna Das <pd10@illinois.edu>
2020-02-26 00:29:29 +0530

    almost done prefixspan

--- CS412/Seq Pattern Mining/prefixspan.py
+++ CS412/Seq Pattern Mining/prefixspan.py
@@ -6,7 +6,7 @@ def read_from_file(filename):
     
     return db
 
-def get_1_freq_itemset(db, minsup):
+def get_freq_itemset(db, minsup):
     item_to_count_map = {}
     for seq in db:
         for item in seq:
@@ -23,7 +23,20 @@ def get_1_freq_itemset(db, minsup):
     return freq_items
 
 def get_projections(db, prefix):
-    return [seq[seq.index(prefix)+1:] for seq in db]
+    new_db = []
+    for seq in db:
+        first_occr = seq.index(prefix)
+        if first_occr < len(seq):
+            new_db.append(seq[first_occr+1:])
+    
+    return new_db
 
 def prefixspan(db, minsup):
+    freq_items = get_freq_itemset(db, minsup)
+    seq_patterns = []
+    for item in freq_items:
+        proj_db = get_projections(db, item)
+        sub_seq_patterns = prefixspan(proj_db, minsup)
+        seq_patterns.append([item] + sub_seq_patterns)
     
+    return seq_patterns
