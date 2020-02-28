977e213a5c8ce2907a9bfb55e20e89c1d7238964
Pradyumna Das <pd10@illinois.edu>
2020-02-28 08:20:12 +0530

    got patterns. support left

--- CS412/Seq Pattern Mining/prefixspan.py
+++ CS412/Seq Pattern Mining/prefixspan.py
@@ -1,3 +1,8 @@
+def write_to_file(patterns):
+    with open("patterns.txt", "w", encoding="utf8") as f:
+        for p in patterns:
+            f.write(";".join(p) + "\n")
+
 def read_from_file(filename):
     db = []
     with open(filename, "r", encoding="utf8") as f:
@@ -16,7 +21,7 @@ def get_freq_itemset(db, minsup):
                 item_to_count_map[item] = 1
     
     freq_items = []
-    for item, count in item_to_count_map.keys():
+    for item, count in item_to_count_map.items():
         if count >= minsup:
             freq_items.append(item)
 
@@ -37,6 +42,11 @@ def prefixspan(db, minsup):
     for item in freq_items:
         proj_db = get_projections(db, item)
         sub_seq_patterns = prefixspan(proj_db, minsup)
-        seq_patterns.append([item] + sub_seq_patterns)
+        seq_patterns = [[item] + fp for fp in sub_seq_patterns]
     
     return seq_patterns
+
+data = read_from_file("reviews_sample.txt")
+rel_min_sup = 0.01
+ans = prefixspan(data, len(data) * rel_min_sup)
+write_to_file(ans)
