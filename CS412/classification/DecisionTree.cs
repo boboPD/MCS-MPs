4e6dd42ad7c56df61d5330f04287950796fb647c
Pradyumna Das <pd10@illinois.edu>
2020-04-18 19:27:21 +0530

    assignment passed!

--- CS412/classification/DecisionTree.cs
+++ CS412/classification/DecisionTree.cs
@@ -1,11 +1,9 @@
 using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 
 class Solution
 {
-
     static byte[,] trainingData, testData;
     static byte[] label;
 
@@ -18,27 +16,33 @@ class Solution
 
         public bool isLeaf = false;
         public byte leafVal;
-
-        public void AddSubtree(int attrValue, Node subtree)
-        {
-            children.Add(attrValue, subtree);
-        }
     }
-
-
 
     static void Main(String[] args)
     {
         ReadTrainingData();
-        Node decisionTree = TrainDecisionTree(Enumerable.Range(0, trainingData.GetLength(0)), Enumerable.Range(0, trainingData.GetLength(1)), 0);
+        Node decisionTree = TrainDecisionTree(Enumerable.Range(0, trainingData.GetLength(0)).ToList(), Enumerable.Range(0, trainingData.GetLength(1)).ToList(), 0);
         ReadTestData();
+        byte[] classificationResults = Classify(decisionTree, testData);
+
+        foreach (var item in classificationResults)
+        {
+            System.Console.WriteLine(item);
+        }
     }
 
     private static Node TrainDecisionTree(List<int> relevantRows, List<int> attrs, int depth)
     {
         Node root = new Node();
 
-        if(attrs.Count == 0 || depth == maxDepth)
+        if (HasOnlyOneClass(relevantRows))
+        {
+            root.isLeaf = true;
+            root.leafVal = label[relevantRows.First()];
+
+            return root;
+        }
+        else if (attrs.Count == 0 || depth == maxDepth)
         {
             root.isLeaf = true;
             root.leafVal = FindMajorityClass(relevantRows);
@@ -50,18 +54,31 @@ class Solution
             root.isLeaf = false;
             root.children = new Dictionary<byte, Node>();
         }
-        
-        int selectedAttr = SelectSplitAttr(relevantRows, attrs);
-        List<int> remainingAttr = RemoveSelectedAttrFromList(attrs, selectedAttr);
-        Dictionary<byte, List<int>> relevantRowsByPartition = GetRelevantRowsForEachPartition(selectedAttr, relevantRows);
+
+        root.attr = SelectSplitAttr(relevantRows, attrs);
+        //System.Console.WriteLine($"Splitting on {root.attr}");
+        List<int> remainingAttr = RemoveSelectedAttrFromList(attrs, root.attr);
+        Dictionary<byte, List<int>> relevantRowsByPartition = GetRelevantRowsForEachPartition(root.attr, relevantRows);
 
         foreach (byte lbl in relevantRowsByPartition.Keys)
         {
-            Node child = TrainDecisionTree(relevantRowsByPartition[lbl], remainingAttr, depth+1);
+            Node child = TrainDecisionTree(relevantRowsByPartition[lbl], remainingAttr, depth + 1);
             root.children.Add(lbl, child);
         }
 
         return root;
+    }
+
+    private static bool HasOnlyOneClass(List<int> rows)
+    {
+        int refIdx = rows.First();
+        foreach (var idx in rows)
+        {
+            if (label[idx] != label[refIdx])
+                return false;
+        }
+
+        return true;
     }
 
     private static List<int> RemoveSelectedAttrFromList(List<int> attributes, int attrToRemove)
@@ -71,21 +88,22 @@ class Solution
         return newList;
     }
 
-    private static byte FindMajorityClass(IEnumerable relevantRows){
+    private static byte FindMajorityClass(List<int> relevantRows)
+    {
         var counts = new Dictionary<byte, int>();
-        foreach (var item in label)
+        foreach (var rowidx in relevantRows)
         {
-            if(counts.ContainsKey(item))
-                ++counts[item];
+            if (!counts.ContainsKey(label[rowidx]))
+                counts.Add(label[rowidx], 1);
             else
-                counts.Add(item, 1);
+                ++counts[label[rowidx]];
         }
 
-        byte majClass;
+        byte majClass = 0;
         int maxCount = -1;
         foreach (var key in counts.Keys)
         {
-            if(counts[key] > maxCount)
+            if (counts[key] > maxCount)
             {
                 maxCount = counts[key];
                 majClass = key;
@@ -95,101 +113,132 @@ class Solution
         return majClass;
     }
 
-    private static Dictionary<byte, List<int>> GetRelevantRowsForEachPartition(int selectedAttr, IEnumerable rows)
+    private static Dictionary<byte, List<int>> GetRelevantRowsForEachPartition(int selectedAttr, List<int> rows)
     {
         var dict = new Dictionary<byte, List<int>>();
         foreach (int rowIdx in rows)
         {
-            if(!dict.ContainsKey(trainingData[rowIdx, selectedAttr]))
+            if (!dict.ContainsKey(trainingData[rowIdx, selectedAttr]))
                 dict.Add(trainingData[rowIdx, selectedAttr], new List<int>());
-            
+
             dict[trainingData[rowIdx, selectedAttr]].Add(rowIdx);
         }
 
         return dict;
     }
 
-    private static int SelectSplitAttr(IEnumerable relevantRows, IEnumerable attrs)
+    private static int SelectSplitAttr(List<int> relevantRows, List<int> attrs)
     {
         var cntByAttrValAndLabel = new Dictionary<byte, Dictionary<byte, int>>();
-        var labelTotalCount = new Dictionary<byte, int>();
+        var attrValTotalCnt = new Dictionary<byte, int>();
         double minGini = double.MaxValue, currGini;
-        int selectedAttr;
+        int selectedAttr = -1;
 
         foreach (int attr in attrs)
         {
+            cntByAttrValAndLabel.Clear();
+            attrValTotalCnt.Clear();
+
             foreach (int idx in relevantRows)
             {
                 if (!cntByAttrValAndLabel.ContainsKey(trainingData[idx, attr]))
                 {
                     cntByAttrValAndLabel.Add(trainingData[idx, attr], new Dictionary<byte, int>());
                     cntByAttrValAndLabel[trainingData[idx, attr]].Add(label[idx], 1);
-                    labelTotalCount.Add(trainingData[idx, attr], 1);
+                    attrValTotalCnt.Add(trainingData[idx, attr], 1);
                 }
                 else
                 {
-                    ++cntByAttrValAndLabel[trainingData[idx, attr]][label[idx]];
-                    ++labelTotalCount(trainingData[idx, attr]);
+                    var temp = cntByAttrValAndLabel[trainingData[idx, attr]];
+                    if (temp.ContainsKey(label[idx]))
+                        ++temp[label[idx]];
+                    else
+                        temp.Add(label[idx], 1);
+                    ++attrValTotalCnt[trainingData[idx, attr]];
                 }
             }
 
+            currGini = 0;
             foreach (byte attrVal in cntByAttrValAndLabel.Keys)
             {
                 double prob = 0.0d;
-                foreach (byte labelVal in cntByAttrValAndLabel[attrVal])
+                foreach (byte labelVal in cntByAttrValAndLabel[attrVal].Keys)
                 {
-                    prob += Math.Pow((double)cntByAttrValAndLabel[attrVal][labelVal] / labelTotalCount[attrVal], 2.0);
+                    prob += Math.Pow((double)cntByAttrValAndLabel[attrVal][labelVal] / attrValTotalCnt[attrVal], 2.0);
                 }
 
-                currGini = (labelTotalCount[attrVal]/(double)relevantRows.Length) * (1 - prob);
-                if(currGini < minGini)
-                {
-                    minGini = currGini;
-                    selectedAttr = attr;
-                }
+                currGini += (attrValTotalCnt[attrVal] / (double)relevantRows.Count) * (1 - prob);
+            }
+
+            if (currGini < minGini)
+            {
+                minGini = currGini;
+                selectedAttr = attr;
             }
         }
 
         return selectedAttr;
     }
 
+    private static byte[] Classify(Node decisionTree, byte[,] data)
+    {
+        var results = new byte[data.GetLength(0)];
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            Node currNode = decisionTree;
+            while (!currNode.isLeaf)
+            {
+                byte selectedAttrVal = data[i, currNode.attr];
+                currNode = currNode.children[selectedAttrVal];
+                
+            }
+
+            results[i] = currNode.leafVal;
+        }
+
+        return results;
+    }
+
     private static void ReadTrainingData()
     {
         int numberOfLines = int.Parse(Console.ReadLine());
-        string[] line = Console.ReadLine().Split(" ");
+        string[] line = Console.ReadLine().Split(' ');
         int numberOfAttrs = line.Length - 1;
 
         trainingData = new byte[numberOfLines, numberOfAttrs];
         label = new byte[numberOfLines];
 
-        for (int i = 1; i < numberOfLines; i++)
+        for (int i = 0; i < numberOfLines; i++)
         {
             label[i] = byte.Parse(line[0]);
             for (int j = 1; j < line.Length; j++)
             {
-                byte val = byte.Parse(line[j].Split(":")[1]);
+                byte val = byte.Parse(line[j].Split(':')[1]);
                 trainingData[i, j - 1] = val;
             }
-            line = Console.ReadLine().Split(" ");
+            if (i < numberOfLines - 1)
+                line = Console.ReadLine().Split(' ');
         }
     }
 
     private static void ReadTestData()
     {
         int numberOfLines = int.Parse(Console.ReadLine());
-        string[] line = Console.ReadLine().Split(" ");
+        string[] line = Console.ReadLine().Split(' ');
         int numberOfAttrs = line.Length;
 
-        var data = new byte[numberOfLines, numberOfAttrs];
+        testData = new byte[numberOfLines, numberOfAttrs];
 
-        for (int i = 1; i < numberOfLines; i++)
+        for (int i = 0; i < numberOfLines; i++)
         {
             for (int j = 0; j < line.Length; j++)
             {
-                byte val = byte.Parse(line[j].Split(":")[1]);
+                byte val = byte.Parse(line[j].Split(':')[1]);
                 testData[i, j] = val;
             }
-            line = Console.ReadLine().Split(" ");
+            if (i < numberOfLines - 1)
+                line = Console.ReadLine().Split(' ');
         }
     }
 }
