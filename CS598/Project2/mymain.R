561c8b69bba90eb9eb210fdf0e86f7f167271d05
Pradyumna Das <pradas@microsoft.com>
2020-11-07 17:53:25 +0530

    preprocess done and separated pred logic into function

--- CS598/Project2/mymain.R
+++ CS598/Project2/mymain.R
@@ -3,24 +3,29 @@ library(tidyverse)
 library(forecast)
 
 preprocess = function(data){
-  clean_data = select(data, Store, Dept, Weekly_Sales, Date)
-  clean_data$Store = as.factor(clean_data$Store)
-  clean_data$Dept = as.factor(clean_data$Dept)
-  clean_data["wk"] = ifelse(year(data$Date) == 2010, week(data$Date)-1, week(data$Date))
-  clean_data
+  new_train = data
+  new_train["wk"] = ifelse(year(data$Date) == 2010, week(data$Date)-1, week(data$Date))
+  new_train
+}
+
+get_preds_snaive = function(trainset, testset){
+  model = snaive(ts(trainset$Weekly_Sales, start = 1, frequency = 52), 8)
+  testset %>% mutate(Weekly_Pred=model$model$future[1:nrow(testset)])
+}
+
+get_preds_spline = function(trainset, testset){
+  
 }
 
 mypredict = function(){
   if (t>1){
-    #new_train = preprocess(new_train)
     train <<- train %>% add_row(new_train)
   }
-  #else{
-  #  train = preprocess(train)
-  #}
   
   start_date <- ymd("2011-03-01") %m+% months(2 * (t - 1))
   end_date <- ymd("2011-05-01") %m+% months(2 * (t - 1))
+  
+  train_current = preprocess(train)
   test_current <- test %>%
     filter(Date >= start_date & Date < end_date) %>%
     select(-IsHoliday)
@@ -29,7 +34,7 @@ mypredict = function(){
   test_pred <- NULL
   
   for(dept in test_depts){
-    train_dept_data <- train %>% filter(Dept == dept)
+    train_dept_data <- train_current %>% filter(Dept == dept)
     test_dept_data <- test_current %>% filter(Dept == dept)
     
     # no need to consider stores that do not need prediction
@@ -41,8 +46,8 @@ mypredict = function(){
     for(store in test_stores){
       subset = train_dept_data %>% filter(Store == store)
       test_subset = test_dept_data %>% filter(Store == store) %>% arrange(Date)
-      model = snaive(ts(subset$Weekly_Sales, start = 1, frequency = 52), 8)
-      temp= test_subset %>% mutate(Weekly_Pred=model$model$future[1:nrow(test_subset)])
+      test_subset = preprocess(test_subset)
+      temp= get_preds_snaive(subset, test_subset)
       if(is.null(test_pred)){
         test_pred = temp
       }
@@ -54,3 +59,4 @@ mypredict = function(){
   
   return(test_pred)
 }
+
