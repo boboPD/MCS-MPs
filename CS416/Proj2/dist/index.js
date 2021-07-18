64530d615ee872d37f34a88b2542699e6de0c620
Pradyumna Das <das_pradyumna@ymail.com>
2021-07-18 23:59:05 +0530

    Chart 1 anootations left

--- CS416/Proj2/dist/index.js
+++ CS416/Proj2/dist/index.js
@@ -1,9 +1,12 @@
-
-function draw() {
-    d3.select("body").select("svg").append("circle").attr("cx", 150).attr("cy", 150).attr("r", 20);
-}
+"use strict";
 function init() {
     sessionStorage.setItem("currentPage", "1");
+    var p1 = fetchChart1Data().then(function (data) { return buildChart1(data); });
+    Promise.all([p1]).then(function () {
+        document.getElementById("loadingPage").hidden = true;
+        document.getElementById("mainPage").hidden = false;
+        d3.select("#page1").classed("hiddenPage", false).classed("currentPage", true);
+    });
 }
 function getCurrentPage() {
     var _a;
@@ -17,9 +20,43 @@ function updatePage(moveDirection) {
     var currentPage = getCurrentPage();
     var nextPage = moveDirection == 'next' ? currentPage + 1 : currentPage - 1;
     d3.select("#page" + currentPage).classed("hiddenPage", true).classed("currentPage", false);
-    d3.select("#page" + nextPage).classed("currentPage", true);
+    d3.select("#page" + nextPage).classed("hiddenPage", false).classed("currentPage", true);
     sessionStorage.setItem("currentPage", nextPage.toString());
     document.getElementById("previousBtn").disabled = !isButtonReq("previous");
     document.getElementById("nextBtn").disabled = !isButtonReq("next");
+}
+function buildChart1(data) {
+    var height = 300, width = 575, padding = 30;
+    var xs = d3.scaleLinear().domain([1700, 2010]).range([0, width]);
+    var ys = d3.scaleLinear().domain([6, 12]).range([height, 0]);
+    var page1svg = d3.select("#page1");
+    var line = d3.line().x(function (d) { return xs(d.Year); }).y(function (d) { return ys(d.temperature); });
+    var temphighs = data.map(function (item) {
+        return [xs(item.Year), ys(item.temperature + item.uncertainty)];
+    });
+    var templows = data.map(function (item) {
+        return [xs(item.Year), ys(item.temperature - item.uncertainty)];
+    });
+    page1svg.append("g").attr("transform", "translate(" + padding + ", " + (height + padding) + ")").call(d3.axisBottom(xs));
+    page1svg.append("g").attr("transform", "translate(" + padding + ", " + padding + ")").call(d3.axisLeft(ys));
+    page1svg.append("g").append("path").attr("d", line(data)).attr("stroke", "red").attr("fill", "none");
+    page1svg.append("g").append("path").attr("d", d3.line()(temphighs)).attr("stroke", "lightgray").attr("fill", "none");
+    page1svg.append("g").append("path").attr("d", d3.line()(templows)).attr("stroke", "lightgray").attr("fill", "none");
+}
+function fetchChart1Data() {
+    var res = [];
+    return d3.csv("https://raw.githubusercontent.com/boboPD/MCS-MPs/master/CS416/Proj2/data/global_avg.csv").then(function (data) {
+        for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+            var item = data_1[_i];
+            if (item["Year"] && item["temperature"] && item["uncertainty"]) {
+                res.push({
+                    Year: parseInt(item["Year"]),
+                    temperature: parseFloat(item["temperature"]),
+                    uncertainty: parseFloat(item["uncertainty"])
+                });
+            }
+        }
+        return res;
+    });
 }
 //# sourceMappingURL=index.js.map
--- CS416/Proj2/index.ts
+++ CS416/Proj2/index.ts
@@ -1,13 +1,13 @@
-import * as d3 from 'd3';
-
 type NavigationState = "next" | "previous";
-
-function draw(){
-    d3.select("body").select("svg").append("circle").attr("cx", 150).attr("cy", 150).attr("r", 20);
-}
 
 function init(): void{
     sessionStorage.setItem("currentPage", "1");
+    const p1 = fetchChart1Data().then(data => buildChart1(data));
+    Promise.all([p1]).then(() => {
+        document.getElementById("loadingPage")!.hidden = true;
+        document.getElementById("mainPage")!.hidden = false;
+        d3.select("#page1").classed("hiddenPage", false).classed("currentPage", true);
+    });
 }
 
 function getCurrentPage(): number {
@@ -24,10 +24,58 @@ function updatePage(moveDirection: NavigationState): void{
     const nextPage = moveDirection == 'next' ? currentPage + 1 : currentPage - 1;
     
     d3.select(`#page${currentPage}`).classed("hiddenPage", true).classed("currentPage", false);
-    d3.select(`#page${nextPage}`).classed("currentPage", true);
+    d3.select(`#page${nextPage}`).classed("hiddenPage", false).classed("currentPage", true);
 
     sessionStorage.setItem("currentPage", nextPage.toString());
 
     (<HTMLButtonElement>document.getElementById("previousBtn")).disabled = !isButtonReq("previous");
     (<HTMLButtonElement>document.getElementById("nextBtn")).disabled = !isButtonReq("next");
+}
+
+function buildChart1(data: Chart1DataModel[]) {
+    const height = 300, width = 575, padding = 30;
+    const xs = d3.scaleLinear().domain([1700, 2010]).range([0, width]);
+    const ys = d3.scaleLinear().domain([6, 12]).range([height, 0]);
+
+    const page1svg = d3.select("#page1");
+    const line = d3.line().x(d => xs(d.Year)).y(d => ys(d.temperature));
+
+    let temphighs: [number, number][] = data.map<[number, number]>(item => {
+        return [xs(item.Year), ys(item.temperature + item.uncertainty)];
+    });
+    let templows: [number, number][] = data.map<[number, number]>(item => {
+        return [xs(item.Year), ys(item.temperature - item.uncertainty)];
+    });
+
+    page1svg.append("g").attr("transform", `translate(${padding}, ${height + padding})`).call(d3.axisBottom(xs));
+    page1svg.append("g").attr("transform", `translate(${padding}, ${padding})`).call(d3.axisLeft(ys));
+    page1svg.append("g").append("path").attr("d", line(data)).attr("stroke", "red").attr("fill", "none");
+    page1svg.append("g").append("path").attr("d", d3.line()(temphighs)).attr("stroke", "lightgray").attr("fill", "none");
+    page1svg.append("g").append("path").attr("d", d3.line()(templows)).attr("stroke", "lightgray").attr("fill", "none");
+}
+
+function fetchChart1Data(): Promise<Chart1DataModel[]> {
+    let res: Chart1DataModel[] = [];
+
+    return d3.csv("https://raw.githubusercontent.com/boboPD/MCS-MPs/master/CS416/Proj2/data/global_avg.csv").then(
+        data => {
+            for (const item of data) {
+                if (item["Year"] && item["temperature"] && item["uncertainty"]) {
+                    res.push({
+                        Year: parseInt(item["Year"]),
+                        temperature: parseFloat(item["temperature"]),
+                        uncertainty: parseFloat(item["uncertainty"])
+                    });
+                }
+            }
+
+            return res;
+        }
+    );
+}
+
+interface Chart1DataModel {
+    Year: number;
+    temperature: number;
+    uncertainty: number;
 }
