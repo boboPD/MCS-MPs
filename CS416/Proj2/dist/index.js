d4b9d653c24a230d0efbfadf7f298a3063697cc4
Pradyumna Das <das_pradyumna@ymail.com>
2021-07-22 01:45:26 +0530

    chart 1 complete

--- CS416/Proj2/dist/index.js
+++ CS416/Proj2/dist/index.js
@@ -26,9 +26,9 @@ function updatePage(moveDirection) {
     document.getElementById("nextBtn").disabled = !isButtonReq("next");
 }
 function buildChart1(data) {
-    var height = 300, width = 575, padding = 30;
-    var xs = d3.scaleLinear().domain([1700, 2010]).range([0, width]);
-    var ys = d3.scaleLinear().domain([6, 12]).range([height, 0]);
+    var height = 300, width = 800, padding = 30;
+    var xs = d3.scaleLinear().domain([1740, 2020]).range([0, width]);
+    var ys = d3.scaleLinear().domain([4, 12]).range([height, 0]);
     var page1svg = d3.select("#page1");
     var line = d3.line().x(function (d) { return xs(d.Year); }).y(function (d) { return ys(d.temperature); });
     var temphighs = data.map(function (item) {
@@ -37,11 +37,44 @@ function buildChart1(data) {
     var templows = data.map(function (item) {
         return [xs(item.Year), ys(item.temperature - item.uncertainty)];
     });
-    page1svg.append("g").attr("transform", "translate(" + padding + ", " + (height + padding) + ")").call(d3.axisBottom(xs));
+    page1svg.append("g").attr("transform", "translate(" + padding + ", " + (height + padding) + ")").call(d3.axisBottom(xs).ticks(28));
     page1svg.append("g").attr("transform", "translate(" + padding + ", " + padding + ")").call(d3.axisLeft(ys));
-    page1svg.append("g").append("path").attr("d", line(data)).attr("stroke", "red").attr("fill", "none");
-    page1svg.append("g").append("path").attr("d", d3.line()(temphighs)).attr("stroke", "lightgray").attr("fill", "none");
-    page1svg.append("g").append("path").attr("d", d3.line()(templows)).attr("stroke", "lightgray").attr("fill", "none");
+    var linegrp = page1svg.append("g").attr("transform", "translate(" + padding + ", " + padding + ")");
+    linegrp.append("g").append("path").attr("d", line(data)).attr("stroke", "blue").attr("fill", "none");
+    linegrp.append("g").append("path").attr("d", d3.line()(temphighs)).attr("stroke", "lightgray").attr("fill", "none");
+    linegrp.append("g").append("path").attr("d", d3.line()(templows)).attr("stroke", "lightgray").attr("fill", "none");
+    var eventData = {
+        1880: ["Industrial revolution", 8.047],
+        1887: ["Production of gasoline powered automobiles", 7.933],
+        1908: ["Mass production of automobiles", 8.202],
+        1939: ["Invention of the jet engine", 8.673],
+        1970: ["Start of the PC age", 8.656]
+    };
+    linegrp.append("g").selectAll("circle").data(Object.keys(eventData)).enter().append("circle")
+        .attr("cx", function (d) { return xs(parseInt(d)); })
+        .attr("cy", function (d) { return ys(eventData[parseInt(d)][1]); })
+        .attr("fill", "red")
+        .attr("r", 3);
+    var annoGrp = page1svg.append("g").attr("transform", "translate(" + padding + ", " + padding + ")");
+    annoGrp.append("g").selectAll("line").data(Object.keys(eventData)).enter().append("line")
+        .attr("x1", function (d) { return xs(parseInt(d)); })
+        .attr("x2", function (d) { return xs(parseInt(d)); })
+        .attr("y1", function (d) { return ys(eventData[parseInt(d)][1]); })
+        .attr("y2", function (d, i) {
+        var factor = i % 2 == 1 ? 1 : -1;
+        var y = ys(eventData[parseInt(d)][1]) + 75 * factor;
+        return i != 2 ? y : y - 50;
+    })
+        .attr("stroke", "black");
+    annoGrp.append("g").selectAll("text").data(Object.keys(eventData)).enter().append("text")
+        .attr("x", function (d) { return xs(parseInt(d) - 15); })
+        .attr("y", function (d, i) {
+        var factor = i % 2 == 1 ? 1 : -1;
+        var y = ys(eventData[parseInt(d)][1]) + 85 * factor;
+        return i != 2 ? y : y - 50;
+    })
+        .html(function (d) { return eventData[parseInt(d)][0]; })
+        .classed("small", true);
 }
 function fetchChart1Data() {
     var res = [];
--- CS416/Proj2/index.ts
+++ CS416/Proj2/index.ts
@@ -1,6 +1,6 @@
 type NavigationState = "next" | "previous";
 
-function init(): void{
+function init(): void {
     sessionStorage.setItem("currentPage", "1");
     const p1 = fetchChart1Data().then(data => buildChart1(data));
     Promise.all([p1]).then(() => {
@@ -33,9 +33,9 @@ function updatePage(moveDirection: NavigationState): void{
 }
 
 function buildChart1(data: Chart1DataModel[]) {
-    const height = 300, width = 575, padding = 30;
-    const xs = d3.scaleLinear().domain([1700, 2010]).range([0, width]);
-    const ys = d3.scaleLinear().domain([6, 12]).range([height, 0]);
+    const height = 300, width = 800, padding = 30;
+    const xs = d3.scaleLinear().domain([1740, 2020]).range([0, width]);
+    const ys = d3.scaleLinear().domain([4, 12]).range([height, 0]);
 
     const page1svg = d3.select("#page1");
     const line = d3.line().x(d => xs(d.Year)).y(d => ys(d.temperature));
@@ -47,11 +47,51 @@ function buildChart1(data: Chart1DataModel[]) {
         return [xs(item.Year), ys(item.temperature - item.uncertainty)];
     });
 
-    page1svg.append("g").attr("transform", `translate(${padding}, ${height + padding})`).call(d3.axisBottom(xs));
+    page1svg.append("g").attr("transform", `translate(${padding}, ${height + padding})`).call(d3.axisBottom(xs).ticks(28));
     page1svg.append("g").attr("transform", `translate(${padding}, ${padding})`).call(d3.axisLeft(ys));
-    page1svg.append("g").append("path").attr("d", line(data)).attr("stroke", "red").attr("fill", "none");
-    page1svg.append("g").append("path").attr("d", d3.line()(temphighs)).attr("stroke", "lightgray").attr("fill", "none");
-    page1svg.append("g").append("path").attr("d", d3.line()(templows)).attr("stroke", "lightgray").attr("fill", "none");
+
+    const linegrp = page1svg.append("g").attr("transform", `translate(${padding}, ${padding})`);
+    linegrp.append("g").append("path").attr("d", line(data)).attr("stroke", "blue").attr("fill", "none");
+    linegrp.append("g").append("path").attr("d", d3.line()(temphighs)).attr("stroke", "lightgray").attr("fill", "none");
+    linegrp.append("g").append("path").attr("d", d3.line()(templows)).attr("stroke", "lightgray").attr("fill", "none");
+
+    // Adding important events data
+    const eventData: {[year: number]: [string, number]} = {
+        1880: ["Industrial revolution", 8.047],
+        1887: ["Production of gasoline powered automobiles", 7.933],
+        1908: ["Mass production of automobiles", 8.202],
+        1939: ["Invention of the jet engine", 8.673],
+        1970: ["Start of the PC age", 8.656]
+    };
+
+    linegrp.append("g").selectAll("circle").data(Object.keys(eventData)).enter().append("circle")
+            .attr("cx", (d) => xs(parseInt(d)))
+            .attr("cy", (d) => ys(eventData[parseInt(d)][1]))
+            .attr("fill", "red")
+            .attr("r", 3);
+    
+    // Adding annotations
+    const annoGrp = page1svg.append("g").attr("transform", `translate(${padding}, ${padding})`);
+    annoGrp.append("g").selectAll("line").data(Object.keys(eventData)).enter().append("line")
+           .attr("x1", (d) => xs(parseInt(d)))
+           .attr("x2", (d) => xs(parseInt(d)))
+           .attr("y1", (d) => ys(eventData[parseInt(d)][1]))
+           .attr("y2", (d, i) => {
+               const factor = i%2 == 1 ? 1 : -1;
+               const y = ys(eventData[parseInt(d)][1]) + 75 * factor
+               return i != 2 ? y : y - 50;
+            })
+           .attr("stroke", "black");
+    annoGrp.append("g").selectAll("text").data(Object.keys(eventData)).enter().append("text")
+           .attr("x", (d) => xs(parseInt(d) - 15))
+           .attr("y", (d, i) => {
+            const factor = i%2 == 1 ? 1 : -1;
+            const y = ys(eventData[parseInt(d)][1]) + 85 * factor
+            return i != 2 ? y : y - 50;
+           })
+           .html((d) => eventData[parseInt(d)][0])
+           .classed("small", true);
+
 }
 
 function fetchChart1Data(): Promise<Chart1DataModel[]> {
